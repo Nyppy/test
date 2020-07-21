c9737c7e5e5a2cfeab600670fa441d9be853fac7
Yarosalw <yarosalw274@gmail.com>
2020-07-21 15:31:32 +0300

    sfdvcx

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,6 @@ module.exports = {
       .rule('images')
         .use('url-loader')
           .loader('url-loader')
-          .tap(options => Object.assign(options, { limit: 10240 }))
+          .tap(options => Object.assign(options, { limit: 102400 }))
   }
 }
